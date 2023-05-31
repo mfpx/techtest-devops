ceeb9d54a5b51cffd0d864017a0553bec46d81df
David <david@stumbra.co.uk>
2023-05-31 19:50:30 +0100

    task 4

---
+++ devops-tf/main.tf
@@ -0,0 +1,27 @@
+variable "kube_config" {
+  type    = string
+  default = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand(var.kube_config)
+    # Below is for explicit configuration, per provider documentation - use if different from local config
+    # Configured for my local cluster, so change it to match your setup
+    # host     = "https://192.168.49.2:8443"
+    # client_certificate     = file("/home/david/.minikube/profiles/minikube/client.crt")
+    # client_key             = file("/home/david/.minikube/profiles/minikube/client.key")
+    # cluster_ca_certificate = file("/home/david/.minikube/ca.crt")
+  }
+}
+
+provider "kubernetes" {
+  config_path = pathexpand(var.kube_config)
+}
+
+# Source: https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release#example-usage---local-chart
+# Note it may take a while for the deployment to finish because of the initialDelaySeconds in the chart
+resource "helm_release" "devops-helm" {
+  name       = "devops-helm"
+  chart      = "../devops-helm"
+}
